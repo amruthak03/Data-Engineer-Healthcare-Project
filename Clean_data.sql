ac2cdfa2c31687b39cdfbf7cc5955c84c20f6e16
amruthak03 <amruthak0307@gmail.com>
2026-02-06 01:11:43 -0500

    updated

--- Clean_data.sql
+++ Clean_data.sql
@@ -3,3 +3,9 @@ CREATE OR REPLACE TABLE `project-15f498fb-28c2-4528-bc7.silver_dataset.providers
 SELECT *
 FROM `project-15f498fb-28c2-4528-bc7.silver_dataset.providers`
 WHERE ProviderID != 'ProviderID';
+
+-- Clean the Departments table in Silver dataset as it has headers/column names as a row
+CREATE OR REPLACE TABLE `project-15f498fb-28c2-4528-bc7.silver_dataset.departments` AS
+SELECT *
+FROM `project-15f498fb-28c2-4528-bc7.silver_dataset.departments`
+WHERE Name != 'Name';
